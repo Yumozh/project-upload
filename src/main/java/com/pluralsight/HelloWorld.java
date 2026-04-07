cb80cb91de73b16aa1c4e535e8d750bfacc1f866
Yumozh <juliiamozhchil@gmail.com>
2026-04-07 15:47:54 -0500

    Print more content in main

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -3,5 +3,6 @@ package com.pluralsight;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Today is a great day.");
+        System.out.println("Spend more time outside!");
     }
 }
